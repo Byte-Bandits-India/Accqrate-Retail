053d94b9561da1f1af0a58921c4230f855bfbedd
luciferckd <www.surendran059@gmail.com>
2025-09-08 14:26:24 +0530

    fix: ther error

--- src/utils/scrollAnimation.js
+++ src/utils/scrollAnimation.js
@@ -1,25 +1,29 @@
 import { useEffect } from "react";
-import { gsap } from "gsap";
-import { ScrollTrigger } from "gsap/ScrollTrigger";
-import { ScrollSmoother } from "gsap/ScrollSmoother";
-
-
-gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
 
 export default function useGsapSmoothScroll() {
   useEffect(() => {
-    // Create ScrollSmoother instance
-    const smoother = ScrollSmoother.create({
-      wrapper: "#smooth-wrapper", 
-      content: "#smooth-content", 
-      smooth: 2.5,  // scroll postion smoothing 
-      effects: true, 
-      smoothTouch: 0.3, // smoothness on touch devices
-    });
+    let smoother;
+
+    if (typeof window !== "undefined") {
+      (async () => {
+        const gsap = (await import("gsap")).default;
+        const { ScrollTrigger } = await import("gsap/ScrollTrigger");
+        const { ScrollSmoother } = await import("gsap/ScrollSmoother");
+
+        gsap.registerPlugin(ScrollTrigger, ScrollSmoother);
+
+        smoother = ScrollSmoother.create({
+          wrapper: "#smooth-wrapper",
+          content: "#smooth-content",
+          smooth: 2.5,
+          effects: true,
+          smoothTouch: 0.3,
+        });
+      })();
+    }
 
     return () => {
-      // cleanup
-      smoother.kill();
+      if (smoother) smoother.kill();
     };
   }, []);
 }
